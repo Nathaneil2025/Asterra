221bcd69f7a86f30ce4b4558df4c084e59bdf6b2
Nathaneil2025 <seanbad85@gmail.com>
2025-08-03 16:41:33 +0300

    gdal + wordpress fix6

--- wordpress.tf
+++ wordpress.tf
@@ -65,3 +65,35 @@ resource "aws_security_group" "wordpress_instance" {
     Name = "WordPress Instance Security Group"
   }
 }
+
+# Security Group for ALB
+resource "aws_security_group" "alb" {
+  name        = "wordpress-alb-sg"
+  description = "Security group for WordPress ALB"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "WordPress ALB Security Group"
+  }
+}
